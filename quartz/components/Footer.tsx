d69f028cfcc5a5110aac65668a2b97ea118bd2c4
overcomersnation <overcomersnation@gmail.com>
2025-10-25 11:47:34 +0000

    footer change

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,7 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
@@ -9,18 +7,22 @@ interface Options {
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        <p>Created by ONC Media</p>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Visit our website:{" "}
+          <a href="https://ebenezerokronipa.com" target="_blank" rel="noopener">
+            ebenezerokronipa.com
+          </a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link}>{text}</a>
+              <a href={link} target="_blank" rel="noopener">
+                {text}
+              </a>
             </li>
           ))}
         </ul>
